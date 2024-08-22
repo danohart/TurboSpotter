ef57d06e45281629101074bc4a924f65845a4435
Daniel Hart <me@danielhart.co>
2024-08-22 11:15:45 -0500

    Move phrases aroudn so newCount can be displayed

--- src/updateScore.js
+++ src/updateScore.js
@@ -1,13 +1,6 @@
 export default async function updateUserScore(interaction, env) {
   const mainUser = `<@${interaction.member.user.id}>`;
   const spottedUser = interaction.data.options[0].value;
-  const phrases = [
-    `Looks like ${mainUser} spotted ${spottedUser}. That's a total of ${newCount} sightings so far.`,
-    `Hey! ${mainUser} has spotted ${spottedUser} again. Bringing your total to ${newCount}!`,
-    `Nice one, ${mainUser}! You've spotted ${spottedUser}. That makes ${newCount} sightings now.`,
-    `Spotted! ${mainUser} has spotted ${spottedUser}. Total sightings: ${newCount}.`,
-    `Great job, ${mainUser}! You've added another spot, seeing ${spottedUser}. That makes ${newCount} sightings in total.`,
-  ];
 
   try {
     const userId = interaction.member.user.id;
@@ -21,6 +14,15 @@ export default async function updateUserScore(interaction, env) {
 
     // Store the updated count back in KV
     await env.SPOTTED_COUNT.put(key, newCount.toString());
+
+    // Define phrases with newCount
+    const phrases = [
+      `Looks like ${mainUser} spotted ${spottedUser}. That's a total of ${newCount} sightings so far.`,
+      `Hey! ${mainUser} has spotted ${spottedUser} again. Bringing your total to ${newCount}!`,
+      `Nice one, ${mainUser}! You've spotted ${spottedUser}. That makes ${newCount} sightings now.`,
+      `Spotted! ${mainUser} has spotted ${spottedUser}. Total sightings: ${newCount}.`,
+      `Great job, ${mainUser}! You've added another spot, seeing ${spottedUser}. That makes ${newCount} sightings in total.`,
+    ];
 
     // Randomly select a phrase
     const randomPhrase = phrases[Math.floor(Math.random() * phrases.length)];
